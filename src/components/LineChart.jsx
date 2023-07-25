0ffc1220893960770f743da5e07f6651d7d97fbd
Nincompup <lit2021032@iiitl.ac.in>
2023-07-25 10:39:03 +0530

    fixed categoryScale issue

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
+import Chart from 'chart.js/auto';
+import {CategoryScale} from 'chart.js'; 
+Chart.register(CategoryScale);
 
 const { Title } = Typography;
 
